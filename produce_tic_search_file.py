b624eb6e1514d3231bb0c2d7abce832440db0876
jessiechristiansen <jessie.christiansen@caltech.edu>
2017-08-28 20:34:45 -0700

    Upload initial script and example input file

---
+++ produce_tic_search_file.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+Jessie Christiansen
+Aug 28 2017
+
+"""
+
+import astropy
+import unicodedata
+from astroquery.simbad import Simbad
+
+lines = [line.rstrip('\n') for line in open('~/nexsci/fulllist.txt')]
+
+f = open('~/nexsci/fulllist_with_coords.txt','w')      
+
+
+for i in range(len(lines)):
+
+    thisName = lines[i]
+    result_table = Simbad.query_object(thisName)
+
+    if result_table is not None:
+        ra_a = result_table.columns[1]
+        ra_b = astropy.table.MaskedColumn.pformat(ra_a)
+        ra_c = ra_b[2]
+        ra_d = unicodedata.normalize('NFKD', ra_c).encode('ascii','ignore')
+        ra_d_new = ra_d.replace(" ", ":")
+        
+        dec_a = result_table.columns[2]
+        dec_b = astropy.table.MaskedColumn.pformat(dec_a)
+        dec_c = dec_b[2]
+        dec_d = unicodedata.normalize('NFKD', dec_c).encode('ascii','ignore')
+        dec_d_new = dec_d.replace(" ", ":")
+        
+        f.write('%s,%s,%s\n' % (thisName, ra_d_new, dec_d_new))
+
+    if result_table is None:
+        print thisName, ' not found'
+
+f.close()
